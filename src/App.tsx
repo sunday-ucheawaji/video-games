fa51e9468e67de1416c5319418649a8a1f8e767f
sunday ucheawaji <sundayucheawaji@gmail.com>
2023-07-27 01:26:23 +0100

    install chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from "react";
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const items: string[] = ["Nigeria", "USA", "London"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Countries"
-        onSelectItem={handleSelectItem}
-      />
-      <br />
-      <Alert>
-        <span>Hello World !</span>
-      </Alert>
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
