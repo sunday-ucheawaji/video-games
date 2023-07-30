958484ac21ea4ecfbd888fc0e9d0713595d041f8
sunday ucheawaji <sundayucheawaji@gmail.com>
2023-07-30 03:04:35 +0100

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+
+export interface Platform {
+id: number;
+name: string;
+slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: {platform: Platform}[]
 }
 
 interface FetchGamesResponse {
@@ -16,7 +23,7 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
-  
+
   useEffect(() => {
     const controller = new AbortController();
 
