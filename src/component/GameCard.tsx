73780405b7a6cd30f36e31cb9555fd69758d19e2
Sunday Ucheawaji <sundayucheawaji@gmail.com>
2023-09-19 09:22:28 +0100

    add emojis

--- src/component/GameCard.tsx
+++ src/component/GameCard.tsx
@@ -5,6 +5,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import GameCardContainer from "./GameCardContainer";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -15,13 +16,15 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <HStack justifyContent="space-between" mb={3} >
+        <HStack justifyContent="space-between" mb={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name} <Emoji rating={game.rating_top} />{" "}
+        </Heading>
       </CardBody>
     </Card>
   );
